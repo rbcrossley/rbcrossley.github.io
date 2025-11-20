7d5a28cb68f53fc1184abb24d7d7cda3e364ce55
rbcrossley <rbcrossley@users.noreply.github.com>
2025-11-20 19:57:10 +0545

    test-sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://rbcrossley.github.io/",
+	site: "https://rbcrossley.github.io",
 	base: "/",
 	integrations: [sitemap({
       entryLimit: 50000,
