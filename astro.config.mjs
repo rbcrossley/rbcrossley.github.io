c6901c249fec616d48a011f0ebae2c00072e73e2
rbcrossley <rbcrossley@users.noreply.github.com>
2026-01-10 10:12:02 +0545

    changed-sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://rbcrossley.github.io",
+	site: "https://bijanaryal.com.np",
 	base: "/",
 	integrations: [sitemap()],
 	markdown: {
